c587aebeafcf4526f6b96fbd2411916d8d3c5abc
Dior1726 <muxammad_909@mail.ru>
2022-12-26 17:58:07 +0600

    feat: create character card and list

--- src/components/RickAndMortyPage/CharacterCard.jsx
+++ src/components/RickAndMortyPage/CharacterCard.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import CardWrapper from '../Card/CardWrapper'
 
 const CharacterCard = ({ character }) => {
-  return <CardWrapper className='p-5'>{character.name}</CardWrapper>
+  return (
+    <CardWrapper className='p-5 flex items-start gap-5'>
+      <div className='w-32 h-32 rounded-[22px] overflow-hidden'>
+        <img
+          className='w-full h-auto'
+          src={character.image}
+          alt={character.name}
+        />
+      </div>
+      <div className='text-sm text-gray-400'>
+        <h3 className='text-lg font-semibold mb-4 text-white'>
+          {character.name}
+        </h3>
+        <div>{character.origin.name}</div>
+        <div>
+          {character.species} / {character.gender} / ({character.status})
+        </div>
+      </div>
+    </CardWrapper>
+  )
 }
 
 export default CharacterCard
