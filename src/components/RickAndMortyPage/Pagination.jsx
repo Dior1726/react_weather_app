72f3b0efe25c260f87509ef859d7a414ca68da85
Dior1726 <muxammad_909@mail.ru>
2022-12-29 11:22:29 +0600

    fefactor: creating first and last numbers pagination

--- src/components/RickAndMortyPage/Pagination.jsx
+++ src/components/RickAndMortyPage/Pagination.jsx
@@ -15,17 +15,46 @@ const Pagination = () => {
 
   return (
     <div className='flex items-center flex-wrap py-5 justify-center gap-3 text-[#cf112b] text-lg'>
+      {currentPage > 3 && (
+        <>
+          <div
+            className='px-2 cursor-pointer font-medium py-1 select-none rounded-md bg-[#191919]'
+            onClick={() => changePageHandler(1)}
+          >
+            1
+          </div>
+          <div>.</div>
+        </>
+      )}
       {pagesCount.map((page, n) => (
         <div
-          className={`px-2 cursor-pointer font-medium py-1 select-none rounded-md ${
-            currentPage === page ? 'bg-[#cf112b] text-white' : 'bg-[#191919]'
-          }`}
+          className={`px-2 cursor-pointer font-medium py-1 select-none rounded-md 
+            ${currentPage === page ? 'bg-[#cf112b] text-white' : 'bg-[#191919]'}
+            ${
+              currentPage + 1 === page ||
+              currentPage - 1 === page ||
+              currentPage === page
+                ? 'block'
+                : 'hidden'
+            }
+          `}
           key={n}
           onClick={() => changePageHandler(page)}
         >
           {page}
         </div>
       ))}
+      {currentPage < 40 && (
+        <>
+          <div>.</div>
+          <div
+            className='px-2 cursor-pointer font-medium py-1 select-none rounded-md bg-[#191919]'
+            onClick={() => changePageHandler(pages)}
+          >
+            {pages}
+          </div>
+        </>
+      )}
     </div>
   )
 }
